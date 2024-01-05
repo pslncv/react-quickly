d5461bf881a4207ec49520c9f2a568b0360ee2f8
Alexander Poselentsev <as.poselentsev@gmail.com>
2024-01-05 17:34:19 +0300

    add chapter #3

---
+++ Chapter #3/jsx/js/script.js
@@ -0,0 +1,1 @@
+ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "Hello from JSX!"), document.getElementById('root'));
---
+++ Chapter #3/jsx/js/script.jsx
@@ -0,0 +1,4 @@
+ReactDOM.render(
+    <h1>Hello from JSX!</h1>,
+    document.getElementById('root')
+)
